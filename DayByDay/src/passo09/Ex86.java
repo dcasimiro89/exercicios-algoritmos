ddd583d2123bfebc4c7bfab553cb61126697bad8
Deivison Savio Casimiro <deivison.savio@gmail.com>
2023-05-31 09:49:34 -0300

    Adicionado a pasta passo09, ex86 e 87

---
+++ DayByDay/src/passo09/Ex86.java
@@ -0,0 +1,16 @@
+package passo09;
+
+public class Ex86 {
+
+    public static void gerador() {
+        System.out.println("+-------=======------+");
+        System.out.println("\tOlá, Mundo!");
+        System.out.println("+-------=======------+");
+    }
+
+    public static void main(String[] args) {
+
+        gerador();
+
+    }
+}
---
+++ DayByDay/src/passo09/Ex87.java
@@ -0,0 +1,22 @@
+package passo09;
+
+import java.util.Scanner;
+
+public class Ex87 {
+
+    public static void gerador(String mensagem) {
+        System.out.println("+-------=======------+");
+        System.out.println("\t" + mensagem);
+        System.out.println("+-------=======------+");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma mensagem: ");
+        String mensagem = sc.nextLine();
+
+        gerador(mensagem);
+        
+    }
+}
